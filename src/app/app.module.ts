a31ed9721235a8d77c51933f7d9d4be1dc21260d
Martin Guevara <guevaramartinjavier@gmail.com>
2022-11-03 16:21:16 -0300

    intentando subir proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header/header.component';
+import { BannerComponent } from './components/banner/banner/banner.component';
+import { ExperienceComponent } from './components/experience/experience/experience.component';
+import { SkillsComponent } from './components/skills/skills/skills.component';
+import { EducationComponent } from './components/education/education/education.component';
+import { ProjectsComponent } from './components/projects/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    EducationComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
